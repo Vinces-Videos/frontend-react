e7b7479c59845ec31abcb2e42b940bd39a0827df
mcoffey-mt <matthew.coffey@madetech.com>
2022-10-10 15:11:32 +0100

    Moved modal into header and centered

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 import PropTypes from 'prop-types'
-import logo from '../logo.png'
 
 //Content for the header, we pass in a modal for the login. This needs to be controlled at the top level so it can overlay all other content on the page
 const Header = ({loginModal}) => {
   return (
-    <header>
-        <img className='logo' src={logo} alt='Vinces Videos Logo'></img>
+    <div className='header'>
+        <h1 className='logo'>Vinces Videos</h1>
         <div className = 'header-right'>
           <button className='login-button' onClick={() => {loginModal(true)}}>Login</button>
         </div>
-    </header>
+    </div>
   )
 }
 
