3c5c7ee4833eb9a29b008e0a1c3bcfb60edaecbf
mcoffey-mt <matthew.coffey@madetech.com>
2022-10-17 16:33:46 +0100

    Implemented MUI UI framework - The table within StaffArea.js has been implemented as an example of what the framework has to offer

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A React front end.
 This front-end variation for Vinces Videos will be using React.
 - React
 - React Router for DOM
-
+- Material UI https://mui.com/ : This has been used to make all UI components look/feel the same without having to spend loads of time messing around with CSS
+- Color Palette https://material.io/inline-tools/color/
 
 ## Setup Notes
 1. The project was set up using Node to create a react app via an `npx` command `npx create-react-app .`. This gave us a template to start from.
--- src/pages/StaffArea.js
+++ src/pages/StaffArea.js
@@ -1,5 +1,7 @@
 //Imports
 import { useState } from 'react';
+import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, Button } from '@mui/material';
+import Paper from '@mui/material/Paper';
 
 //Components=
 import NewMovie from '../components/NewMovie'
@@ -19,70 +21,72 @@ export default function StaffArea({movies}){
                 <p>navigation panel for staff area, current view is movies</p>
             </div>
             <div className='body-container'>
-                <p>You are in a restricted area. There should have been some form of authentication to prevent this!</p>
-                <button onClick={() => setNewMovieState(true)}>New</button>
+                <p>You are in a restricted area. This should have been some form of authentication to prevent this!</p>
+                <Button onClick={() => setNewMovieState(true)}>New</Button>
                 {newMovieState && <NewMovie displayForm={setNewMovieState}/>}
-                <button onClick={() => deleteSelected()}>Delete Selected</button>
+                <Button onClick={() => deleteSelected()}>Delete Selected</Button>
                 <label>Edit Mode</label>
                 <input type='checkbox'></input>
-                <table>
-                    <thead>
-                        <tr className="table-header">
-                            <th>-</th>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Age Rating</th>
-                            <th>Archived</th>
-                            <th>Duration (minutes)</th>
-                            <th>Stock Count</th>
-                            <th>Thumbnail URL</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {movies.map(element => {
-                            //The key property is just for React, we need to assign our own accessible key to be able to identify the record.
-                            return (
-                                <tr key={element.id}>
-                                    <td>
-                                        <input data-key={element.id} className='toDelete' type='checkbox'></input>
-                                    </td>
-                                    <td>{element.name}</td>
-                                    <td>{element.category}</td>
-                                    <td>{element.ageRating}</td>
-                                    <td>
-                                        <input type='checkbox' disabled={true} defaultChecked={element.archived} ></input>
-                                    </td>
-                                    <td>{element.durationMinutes}</td>
-                                    <td>{element.stockCount}</td>
-                                    <td>{element.thumbnailUrl}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>-</TableCell>
+                                <TableCell>Title</TableCell>
+                                <TableCell>Category</TableCell>
+                                <TableCell>Age Rating</TableCell>
+                                <TableCell>Archived</TableCell>
+                                <TableCell>Duration (minutes)</TableCell>
+                                <TableCell>Stock Count</TableCell>
+                                <TableCell>Thumbnail URL</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {movies.map(element => {
+                                //TableCelle key property is just for React, we need to assign our own accessible key to be able to identify TableCelle record.
+                                return (
+                                    <TableRow key={element.id}>
+                                        <TableCell>
+                                            <input data-key={element.id} className='toDelete' type='checkbox'></input>
+                                        </TableCell>
+                                        <TableCell>{element.name}</TableCell>
+                                        <TableCell>{element.category}</TableCell>
+                                        <TableCell>{element.ageRating}</TableCell>
+                                        <TableCell>
+                                            <input type='checkbox' disabled={true} defaultChecked={element.archived} ></input>
+                                        </TableCell>
+                                        <TableCell>{element.durationMinutes}</TableCell>
+                                        <TableCell>{element.stockCount}</TableCell>
+                                        <TableCell>{element.thumbnailUrl}</TableCell>
+                                    </TableRow>
+                                )
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         </div>
     )
 
 
-    //Use query selector to get the attribute from ticked to delete rows
+    //Use query selector to get TableCelle atTableRowibute from ticked to delete rows
     function deleteSelected(){
         const deleteElements = document.querySelectorAll('input[type=checkbox]:checked.toDelete');
         //Convert to an array and drop all unnecessary additional properties
-        const deleteKeys = [...deleteElements].map(htmlElement => htmlElement.getAttribute('data-key'));
+        const deleteKeys = [...deleteElements].map(htmlElement => htmlElement.getAtTableRowibute('data-key'));
         console.log('Keys to delete are', deleteKeys);
-        //Ensure the user that they want to do this, multiple rows will be deleted!
-        if (deleteKeys.length > 0)
-            if (window.confirm(`Are you sure you want to delete ${deleteKeys.length} records`))
+        //Ensure TableCelle user TableCellat TableCelley want to do TableCellis, multiple rows will be deleted!
+        if (deleteKeys.lengTableCell > 0)
+            if (window.confirm(`Are you sure you want to delete ${deleteKeys.lengTableCell} records`))
             {
                 //API only currently supports deleting one key at a time
                 deleteKeys.forEach(key => Api.Delete(process.env.REACT_APP_productsEndpoint, key, (result) => {
                     console.log(result);
-                    //This is messy, shouldn't require a whole page refresh
+                    //TableCellis is messy, shouldn't require a whole page refresh
                     window.location.reload();
                 }));
 
-                //proceed with delete
+                //proceed wiTableCell delete
                 console.log('deleting');
             }
     }
